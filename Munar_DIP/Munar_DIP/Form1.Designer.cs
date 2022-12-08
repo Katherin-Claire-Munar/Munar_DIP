4b2945059421d7aea53b8d99ba63de5ea11e6301
Katherin Claire Munar <111827954+Katherin-Claire-Munar@users.noreply.github.com>
2022-12-08 13:13:17 +0800

    Added code file for Activity 1
    - Implemented Basic Copy 
    - Implemented Greyscale 
    -Implemented Color Inversion 
    -Implemented Histogram 
    -Implemented Sepia

---
+++ Munar_DIP/Munar_DIP/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Munar_DIP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnSepia = new System.Windows.Forms.Button();
+            this.btnHistogram = new System.Windows.Forms.Button();
+            this.btnNegative = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnGrayscale = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(31, 45);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(360, 295);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(412, 45);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(383, 295);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(31, 348);
+            this.btnLoad.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(100, 28);
+            this.btnLoad.TabIndex = 5;
+            this.btnLoad.Text = "Load ";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(412, 348);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(100, 28);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnSepia
+            // 
+            this.btnSepia.Location = new System.Drawing.Point(825, 181);
+            this.btnSepia.Name = "btnSepia";
+            this.btnSepia.Size = new System.Drawing.Size(132, 26);
+            this.btnSepia.TabIndex = 16;
+            this.btnSepia.Text = "Sepia";
+            this.btnSepia.UseVisualStyleBackColor = true;
+            this.btnSepia.Click += new System.EventHandler(this.btnSepia_Click);
+            // 
+            // btnHistogram
+            // 
+            this.btnHistogram.Location = new System.Drawing.Point(825, 149);
+            this.btnHistogram.Name = "btnHistogram";
+            this.btnHistogram.Size = new System.Drawing.Size(131, 26);
+            this.btnHistogram.TabIndex = 15;
+            this.btnHistogram.Text = "Histogram";
+            this.btnHistogram.UseVisualStyleBackColor = true;
+            this.btnHistogram.Click += new System.EventHandler(this.btnHistogram_Click);
+            // 
+            // btnNegative
+            // 
+            this.btnNegative.Location = new System.Drawing.Point(825, 114);
+            this.btnNegative.Margin = new System.Windows.Forms.Padding(4);
+            this.btnNegative.Name = "btnNegative";
+            this.btnNegative.Size = new System.Drawing.Size(131, 28);
+            this.btnNegative.TabIndex = 14;
+            this.btnNegative.Text = "Color Inversion";
+            this.btnNegative.UseVisualStyleBackColor = true;
+            this.btnNegative.Click += new System.EventHandler(this.btnNegative_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(825, 45);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(130, 26);
+            this.btnCopy.TabIndex = 13;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // btnGrayscale
+            // 
+            this.btnGrayscale.Location = new System.Drawing.Point(825, 78);
+            this.btnGrayscale.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGrayscale.Name = "btnGrayscale";
+            this.btnGrayscale.Size = new System.Drawing.Size(130, 28);
+            this.btnGrayscale.TabIndex = 12;
+            this.btnGrayscale.Text = "Grayscale";
+            this.btnGrayscale.UseVisualStyleBackColor = true;
+            this.btnGrayscale.Click += new System.EventHandler(this.btnGrayscale_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(977, 405);
+            this.Controls.Add(this.btnSepia);
+            this.Controls.Add(this.btnHistogram);
+            this.Controls.Add(this.btnNegative);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnGrayscale);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Munar Digital Image Processing ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnSepia;
+        private System.Windows.Forms.Button btnHistogram;
+        private System.Windows.Forms.Button btnNegative;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnGrayscale;
+    }
+}
+
---
+++ Munar_DIP/Munar_DIP/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Munar_DIP
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog openFile;
+        SaveFileDialog saveFile;
+        Bitmap newImage, convertedImage;
+        public Form1()
+        {
+            InitializeComponent();
+
+            openFile = new OpenFileDialog();
+            openFile.RestoreDirectory = true;
+            openFile.InitialDirectory = "C:\\";
+            openFile.FilterIndex = 1;
+            openFile.Filter = "jpg Files (*.jpg)|*.jpg|gif Files (*.gif)|*.gif|png Files (*.png)|*.png ";
+
+            saveFile = new SaveFileDialog();
+            saveFile.RestoreDirectory = true;
+            saveFile.InitialDirectory = "C:\\";
+            saveFile.FilterIndex = 1;
+            saveFile.Filter = "jpg Files (*.jpg)|*.jpg|gif Files (*.gif)|*.gif|png Files (*.png)|*.png";
+          
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.OK == saveFile.ShowDialog())
+            {
+                Image img = (Image)convertedImage;
+                img.Save(saveFile.FileName);
+            }
+        }
+
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.OK == openFile.ShowDialog())
+            {
+                newImage = new Bitmap(openFile.FileName);
+                pictureBox1.Image = newImage;
+            }
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            convertedImage = new Bitmap(newImage.Width, newImage.Height);
+            for (int x = 0; x < newImage.Width; x++)
+            {
+                for (int y = 0; y < newImage.Height; y++)
+                {
+                    Color p = newImage.GetPixel(x, y);
+                    convertedImage.SetPixel(x, y, p);
+                }
+            }
+            pictureBox2.Image = convertedImage;
+        }
+
+
+        private void btnGrayscale_Click(object sender, EventArgs e)
+        {
+            convertedImage = new Bitmap(newImage.Width, newImage.Height);
+            for (int x = 0; x < newImage.Width; x++)
+            {
+                for (int y = 0; y < newImage.Height; y++)
+                {
+                    Color p = newImage.GetPixel(x, y);
+                    int grey = (p.R + p.G + p.B) / 3;
+                    convertedImage.SetPixel(x, y, Color.FromArgb(grey, grey, grey));
+                }
+            }
+            pictureBox2.Image = convertedImage;
+        }
+
+
+        private void btnNegative_Click(object sender, EventArgs e)
+        {
+            convertedImage = new Bitmap(newImage.Width, newImage.Height);
+            for (int x = 0; x < newImage.Width; x++)
+            {
+                for (int y = 0; y < newImage.Height; y++)
+                {
+                    Color p = newImage.GetPixel(x, y);
+                    int grey = (p.R + p.G + p.B) / 3;
+                    convertedImage.SetPixel(x, y, Color.FromArgb(255 - p.R, 255 - p.G, 255 - p.B));
+                }
+            }
+            pictureBox2.Image = convertedImage;
+        }
+
+        private void btnHistogram_Click(object sender, EventArgs e)
+        {
+            Color p;
+
+            for (int x = 0; x < newImage.Width; x++)
+            {
+                for (int y = 0; y < newImage.Height; y++)
+                {
+                    p = newImage.GetPixel(x, y);
+                    int grey = (p.R + p.G + p.B) / 3;
+                    newImage.SetPixel(x, y, Color.FromArgb(grey, grey, grey));
+                }
+            }
+
+            int[] hisdata = new int[256];
+            for (int x = 0; x < newImage.Width; x++)
+            {
+                for (int y = 0; y < newImage.Height; y++)
+                {
+                    p = newImage.GetPixel(x, y);
+                    hisdata[p.R]++;
+                }
+            }
+
+            convertedImage = new Bitmap(256, 800);
+            for (int x = 0; x < 256; x++)
+            {
+                for (int y = 0; y < 800; y++)
+                {
+                    convertedImage.SetPixel(x, y, Color.White);
+                }
+            }
+
+            for (int x = 0; x < 256; x++)
+            {
+                for (int y = 0; y < Math.Min(hisdata[x] / 5, convertedImage.Height - 1); y++)
+                {
+                    convertedImage.SetPixel(x, (convertedImage.Height - 1) - y, Color.Black);
+                }
+            }
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.Image = convertedImage;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSepia_Click(object sender, EventArgs e)
+        {
+            convertedImage = new Bitmap(newImage.Width, newImage.Height);
+            for (int x = 0; x < newImage.Width; x++)
+            {
+                for (int y = 0; y < newImage.Height; y++)
+                {
+                    Color p = newImage.GetPixel(x, y);
+                    int r = (int)(0.393 * p.R + 0.769 * p.G + 0.189 * p.B);
+                    int g = (int)(0.349 * p.R + 0.686 * p.G + 0.168 * p.B);
+                    int b = (int)(0.272 * p.R + 0.534 * p.G + 0.131 * p.B);
+
+                    if (r > 255)
+                    {
+                        r = 255;
+                    }
+
+                    if (g > 255)
+                    {
+                        g = 255;
+                    }
+
+                    if (b > 255)
+                    {
+                        b = 255;
+                    }
+
+                    convertedImage.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+            }
+            pictureBox2.Image = convertedImage;
+        }
+
+
+
+
+
+    }
+}
